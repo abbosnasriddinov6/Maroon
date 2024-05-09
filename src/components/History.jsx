27b4fa2c79579ed04822e5a542baa7ae68e00359
abbosnasriddinov6 <abbosbeknasriddinov001@gmail.com>
2024-05-09 12:09:25 +0500

    about

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -7,7 +7,7 @@ const History = () => {
                 <div className='boychechak'>
                     <h1>“We strive to make skin care accessible
                         and a  pleasant <br /> ritual for everyone who wants
-                        take care <br />  of yourself and your body”</h1>
+                        take care about you <br />  of yourself and your body”</h1>
                     <button>Our history</button>
                 </div>
             </div>
